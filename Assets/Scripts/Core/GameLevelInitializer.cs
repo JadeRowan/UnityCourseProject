698464cbfb3c37787d4a4962d85a46488bf55923
JadeRowan <rougesoul.m@gmail.com>
2023-05-12 15:27:13 +0300

    Lab4-5 Fixing animations and adding UI input elements

---
+++ Assets/Scripts/Core/GameLevelInitializer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Player;
+using UnityEngine;
+
+namespace Core
+{
+    public class GameLevelInitializer : MonoBehaviour
+    {
+        [SerializeField] private PlayerEntity _playerEntity;
+        [SerializeField] private GameUiInputView _gameUiInputView;
+
+        private ExternalDevicesInputReader _externalDevicesInput;
+        private PlayerBrain _playerBrain;
+
+        private bool _onPause;
+
+        private void Awake()
+        {
+            _externalDevicesInput = new ExternalDevicesInputReader();
+            _playerBrain = new PlayerBrain(_playerEntity, new List<IEntityInputSource>
+            {
+                _gameUiInputView,
+                _externalDevicesInput
+            });
+        }
+
+        private void FixedUpdate()
+        {
+            if (_onPause)
+                return;
+
+            _playerBrain.OnFixedUpdate();
+        }
+
+        private void Update()
+        {
+            if (_onPause) 
+                return;
+
+
+            _externalDevicesInput.OnUpdate();
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerAnimations/UnityAnimatorController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Player.PlayerAnimations
+{
+    public class UnityAnimatorController : AnimationController
+    {
+        private Animator _animator;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        protected override void PlayAnimation(AnimationType animationType)
+        {
+            
+            _animator.SetInteger(nameof(AnimationType), (int)animationType);
+        }
+    }
+}
